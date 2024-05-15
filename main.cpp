7fb657d0b51b7830de83af786159b25321cd5da0
bittiboop <sstrelkova15@gmail.com>
2024-05-15 20:25:16 +0300

    added main code for homework

--- main.cpp
+++ main.cpp
@@ -5,16 +5,26 @@ using namespace std;
 int main() {
 	
 	const int SIZE = 10;
-	//init
 	int arr[SIZE];
+	int min = 0, max = 0;
 	srand(time(0));
-	//fill array with random numbers
-	//some bl
-	//result
+	cout << "Array: ";
 	for (size_t i = 0;i < SIZE;i++) {
 		arr[i] = rand() % 20 - 10;
 		cout << arr[i] << " ";
 	}
+	cout << endl;
+	min = max = arr[0];
+	for (size_t i = 1; i < SIZE; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	cout << "Min: " << min << endl;
+	cout << "Max: " << max << endl;
 	cout << endl;
 	return 0;
 }
