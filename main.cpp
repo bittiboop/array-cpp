4e6a6851d45c7d314b5fc6eb473c1e5227ac8c16
bittiboop <sstrelkova15@gmail.com>
2024-05-15 20:42:27 +0300

    added first ver of code

--- main.cpp
+++ main.cpp
@@ -2,19 +2,40 @@
 #include <ctime>
 using namespace std;
 
-int main() {
-	
-	const int SIZE = 10;
-	//init
-	int arr[SIZE];
-	srand(time(0));
-	//fill array with random numbers
-	//some bl
-	//result
-	for (size_t i = 0;i < SIZE;i++) {
-		arr[i] = rand() % 20 - 10;
-		cout << arr[i] << " ";
+int main() 
+{
+	int min, max;
+	const int months = 12;
+	double profits[months];
+
+	for (size_t i = 0; i < months; i++) {
+		cout << "Enter the price from " << i + 1 << " month: ";
+		cin >> profits[i];
 	}
+	min = max = profits[0];
+	for (size_t i = 0; i < months; i++) {
+		if (profits[i] < min) {
+			min = i;
+		}
+		if (profits[i] > max) {
+			max = i;
+		}
+	}
+	int num1, num2;
+	cout << "Enter the start range of months: ";
+	cin >> num1;
+	cout << "Enter the end range of months: ";
+	cin >> num2;
+	
+	for (size_t i = num1; i < num2; i++) {
+		while (months < num2 && months > num1) {
+			cout << "The minimum profit was in the " << min + 1 << " month: " << profits[min] << endl;
+			cout << "The maximum profit was in the " << max + 1 << " month: " << profits[max] << endl;
+		}break;
+	}
+	
+	
+	
 	cout << endl;
 	return 0;
 }
