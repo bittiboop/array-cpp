ae6d9b802a8f32b905e74c11d47795938c5f7521
bittiboop <sstrelkova15@gmail.com>
2024-05-15 20:22:09 +0300

    added code

--- main.cpp
+++ main.cpp
@@ -4,16 +4,25 @@ using namespace std;
 
 int main() {
 	
-	const int SIZE = 5;
-	int summ = 0;
-	int arr[SIZE];
-	for (size_t i = 0;i < SIZE;i++) {
-		cout << "Enter length of 5 sides: ";
-		cin >> arr[i];
-	}
-	summ = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+	int min, max;
+	const int months = 12;
+	double profits[months];
 	
-	cout << summ << " ";
+	for (size_t i = 0;i < months;i++) {
+		cout << "Enter the price from " << i + 1 << " month: ";
+		cin >> profits[i];
+	}
+	min = max = profits[0];
+	for (size_t i = 0; i < months; i++) {
+		if (profits[i] < min) {
+			min = i;
+		}
+		if (profits[i] > max) {
+			max = i;
+		}
+	}	
+	cout << "The minimum profit was in the " << min + 1 << " month: " << profits[min] << endl;
+	cout << "The maximum profit was in the " << max + 1 << " month: " << profits[max] << endl;
 	cout << endl;
 	return 0;
 }
