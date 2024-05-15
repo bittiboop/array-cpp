ccc07f159c2ec6fc10c5db33768395b1676b564d
bittiboop <sstrelkova15@gmail.com>
2024-05-15 19:12:57 +0300

    made ex 1

--- main.cpp
+++ main.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 int main() {
 	
-	const int SIZE = 10;
-	//init
+	const int SIZE = 6;
+	const int array_2 = 6;
 	int arr[SIZE];
 	srand(time(0));
-	//fill array with random numbers
-	//some bl
-	//result
-	for (size_t i = 0;i < SIZE;i++) {
-		arr[i] = rand() % 20 - 10;
+	for (size_t i = 0; i < SIZE; i++) {
+		cout << "Enter this month value: ";
+		cin >> arr[i];
+	}
+	cout << "Sixth month value is: ";
+	for (size_t i = 0; i < SIZE; i++)
+	{
 		cout << arr[i] << " ";
 	}
+	
 	cout << endl;
 	return 0;
 }
