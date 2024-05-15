d4dc195ddeb66fba9fbfe16ee97039ebf5f23c33
bittiboop <sstrelkova15@gmail.com>
2024-05-15 18:49:54 +0300

    added lesson code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main() {
 	
-
+	const int SIZE = 10;
+	//init
+	int arr[SIZE];
+	srand(time(0));
+	//fill array with random numbers
+	//some bl
+	//result
+	for (size_t i = 0;i < SIZE;i++) {
+		arr[i] = rand() % 20 - 10;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 	return 0;
 }
